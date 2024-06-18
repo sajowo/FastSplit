92fc86b0f2978116983535b51a06fb7c01d01bc3
Bartosz Kawalec <bartekka2003@gmail.com>
2024-06-18 14:04:04 +0200

    Zmiany w celu poprawnego dzialania bazy danych

--- EsSplit/models.py
+++ EsSplit/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    
+class Group(models.Model):
+    name = models.CharField(max_length=30)
+    members = models.ManyToManyField(Person, through='Membership')
+    
+class Membership(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    date_joined = models.DateField()
