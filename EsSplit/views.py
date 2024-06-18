9999b22ac19d458b2e92e3b9920d972b3a9f171e
Bartosz Kawalec <bartekka2003@gmail.com>
2024-06-18 14:42:47 +0200

    Dodanie polaczenia pomiedzy formularzem rejestracyjnym a baza danych

--- EsSplit/views.py
+++ EsSplit/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import RegisterForm
 import logging
+import random
 
 
 logger = logging.getLogger(__name__)
@@ -31,16 +34,24 @@ def logout_view(request):
     logout(request)
     return redirect('login') 
 
+def generate_username(first_name, last_name):
+    base_username = f"{first_name.lower()}{last_name.lower()}"
+    username = base_username
+    while User.objects.filter(username=username).exists():
+        username = f"{base_username}{random.randint(1000, 9999)}"
+    return username
+
+
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            messages.success(request, "Konto zostało utworzone! Zaloguj się.")
-            return redirect(reverse('login'))
-        else:
-            messages.error(request, "Błąd podczas tworzenia konta.")
-            return render(request, 'signup.html', {'form': form})
+            user = form.save(commit=False)
+            user.username = generate_username(user.first_name, user.last_name)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            login(request, user)
+            return redirect('index')  # przekierowanie do strony głównej lub innej strony po rejestracji
     else:
-        form = UserCreationForm()
-        return render(request, 'signup.html', {'form': form})
+        form = RegisterForm()
+    return render(request, 'SignUp.html', {'form': form})
